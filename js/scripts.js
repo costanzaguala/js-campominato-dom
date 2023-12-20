48c4053cf8f8ff3338cbb6fb7c41a823c53a6ce5
costanzaguala <costanzasilviaguala@gmail.com>
2023-12-20 09:29:47 +0100

    esercizio fatto

--- js/scripts.js
+++ js/scripts.js
@@ -6,35 +6,54 @@ Ogni cella ha un numero progressivo, da 1 a 100.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
 */
+const buttonStart = document.getElementById('start');
+const container = document.querySelector('.container');
 
-const buttonStart = document.getElementById ('start');
-console.log(buttonStart);
-
-const container = document.querySelector ('.container');
-console.log(container);
-
-buttonStart.addEventListener ("click", function (){
-
+buttonStart.addEventListener("click", function () {
     let grid = document.createElement('div');
     grid.classList.add('grid-numbers');
-    container.append(grid);
-    console.log(grid);
+    container.appendChild(grid);
 
-    for ( i = 1; i <= 100 ; i++ ) {
+    buttonStart.classList.add('button-start-display');
 
-        const cell = document.createElement ('div');
+    const bombs = generateBombs();
+    const maxClicks = 100 - bombs.length;
+    let clicks = 0;
+
+    for (let i = 1; i <= 100; i++) {
+        const cell = document.createElement('div');
         cell.classList.add('cell-style');
-        cell.innerHTML= i;
-        grid.append(cell);
-        console.log(cell);
+        cell.innerHTML = i;
+        grid.appendChild(cell);
 
-        buttonStart.classList.add('button-start-display');
-
-        cell.addEventListener ("click", function (){
-            cell.classList.toggle('cell-color');
-        })
-
-        console.log(cell[i]);
+        cell.addEventListener("click", function () {
+            if (bombs.includes(i)) {
+                cell.classList.add('cell-bomb');
+                endGame(false);
+            } else {
+                cell.classList.add('cell-color');
+                clicks++;
+                if (clicks === maxClicks) {
+                    endGame(true);
+                }
+            }
+        });
     }
 
-})
+    function generateBombs() {
+        const bombs = [];
+        while (bombs.length < 16) {
+            const bomb = Math.floor(Math.random() * 100) + 1;
+            if (!bombs.includes(bomb)) {
+                bombs.push(bomb);
+            }
+        }
+        return bombs;
+    }
+
+    function endGame(isWin) {
+        const message = isWin ? "Hai vinto!" : "Hai perso!";
+        alert(`${message}\nPunteggio: ${isWin ? "Massimo" : "Sconfitta"}`);
+        buttonStart.classList.add('button-start-display');
+    }
+});
